fd796854fc0690980123972a3532529fa0aaf595
jung tae wook <xodnr901901@naver.com>
2024-07-08 14:25:15 +0900

    1

--- backend/mybuild.sh
+++ backend/mybuild.sh
@@ -6,24 +6,24 @@ npm run build
 cd ../backend
 rm -rf src/main/resources/static
 mv ../frontend/dist src/main/resources/static
-cp -r ../frontend/public src/main/resources/static
+cp -r ../frontend/public src/main/resources/static/
 
 # spring project build
 ./gradlew bootJar
 
 # build image
-docker build -t jeonghyeon123/alba-connector .
+docker build -t jungtaewook/arbeit .
 
 # push image
-docker push jeonghyeon123/alba-connector
+docker push jungtaewook/arbeit
 
 # remote 에서
 
 # 컨테이너 멈추고
-ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.25.153 'docker stop alba-connector'
+ssh -i src/main/resources/secret/key0507.pem ubuntu@54.180.9.228 'docker stop prj3'
 # 컨테이너 삭제
-ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.25.153 'docker rm alba-connector'
+ssh -i src/main/resources/secret/key0507.pem ubuntu@54.180.9.228 'docker rm prj3'
 # pull image
-ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.25.153 'docker pull jeonghyeon123/alba-connector'
+ssh -i src/main/resources/secret/key0507.pem ubuntu@54.180.9.228 'docker pull jungtaewook/arbeit'
 # 컨테이너 실행
-ssh -i src/main/resources/secret/key0527.pem ubuntu@43.201.25.153 'docker run -d -p 8080:8080 --restart always --name alba-connector jeonghyeon123/alba-connector'
+ssh -i src/main/resources/secret/key0507.pem ubuntu@54.180.9.228 'docker run -d -p 8080:8080 --restart always --name prj3 jungtaewook/arbeit'
